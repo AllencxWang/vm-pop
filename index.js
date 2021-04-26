3b6d088feebd837044b355ed17179c9500579f3e
Allen Wang <allen.wang@ringcentral.com>
2021-04-26 23:02:45 +0800

    update selector

--- index.js
+++ index.js
@@ -16,30 +16,39 @@ const jsonFile = path.join(__dirname, `file-list/${cred.courseName}.json`);
 (async () => {
   if (!fs.existsSync(jsonFile)) {
     await (async () => {
-      const browser = await puppeteer.launch({devtools: true});
+      const browser = await puppeteer.launch({
+        // devtools: true,
+        // headless: false,
+        args: ['--disable-features=site-per-process'], // for intercept iframe vimeo response
+      });
       const page = await browser.newPage();
       await page.goto(cred.homepage);
-      await page.click('#__layout > div > header > button');
+      await page.click('#__layout > div > div > div > header > button');
       await page.waitFor(2000);
-      await page.click('#__layout > div > header > div > nav > div.navbar-secondary.notlogged > button.button.primary.-small');
+      await page.click(
+        '#__layout > div > div > div > header > div > nav > div.navbar-secondary.notlogged > button.button.primary.-small'
+      );
       await page.waitFor(2000);
       await page.type('input[type="email"]', cred.username);
       await page.type('input[type="password"]', cred.password);
-      await page.click('button[type="submit"]');
+      await page.click(
+        '#__layout > div > div.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div.form-actions > button'
+      );
       await page.waitFor(2000);
       await page.goto(cred.courseUrl);
-    
+      await page.waitFor(2000);
       const lessons = await page.evaluate(() => {
-        const titles = Array.from(document.querySelectorAll('.list-item-title'));
-        return titles.map(title => title.innerText.replace(/&/g, 'and'));
+        const titles = Array.from(
+          document.querySelectorAll('.list-item-title')
+        );
+        return titles.map((title) => title.innerText.replace(/&/g, 'and'));
       });
-    
+
       const lessonTable = {};
-    
+
       for (let i = 0; i < lessons.length; i++) {
         const lesson = lessons[i];
-        await page.click(`.list-item:nth-of-type(${i+1})`);
-    
+        await page.click(`.list-item:nth-of-type(${i + 1})`);
         await new Promise(async (resolve) => {
           console.log('Lesson - ', lesson);
           page.on('response', async (response) => {
@@ -47,11 +56,13 @@ const jsonFile = path.join(__dirname, `file-list/${cred.courseName}.json`);
             if (root.includes('master.json')) {
               const data = await response.json();
               root = path.join(
-                root.substring(0, root.lastIndexOf('/')), data.base_url);
-              
+                root.substring(0, root.lastIndexOf('/')),
+                data.base_url
+              );
+
               const audio = data.audio[0];
               let video = null;
-        
+
               let highestBitrate = 0;
               for (let v of data.video) {
                 if (v.width === 1920 && v.height === 1080) {
@@ -62,22 +73,31 @@ const jsonFile = path.join(__dirname, `file-list/${cred.courseName}.json`);
                   highestBitrate = v.bitrate;
                   video = v;
                 }
+                console.log('video:', v);
               }
-        
+
               const getFileLinks = (segments, baseUrl) =>
                 // 'segment-0.m4s' is actually a hidden (un-listed) file
-                [{url: 'segment-0.m4s'}].concat(segments).reduce((accu, curr) => {
-                  accu.push({file: curr.url, url: path.join(baseUrl, curr.url)});
-                  return accu;
-                }, []);
-              
-    
+                [{ url: 'segment-0.m4s' }]
+                  .concat(segments)
+                  .reduce((accu, curr) => {
+                    accu.push({
+                      file: curr.url,
+                      url: path.join(baseUrl, curr.url),
+                    });
+                    return accu;
+                  }, []);
+
               lessonTable[lesson] = {
                 audio: getFileLinks(
-                  audio.segments, path.join(root, audio.base_url)),
+                  audio.segments,
+                  path.join(root, audio.base_url)
+                ),
                 video: getFileLinks(
-                  video.segments, path.join(root, video.base_url))
-              }
+                  video.segments,
+                  path.join(root, video.base_url)
+                ),
+              };
               resolve();
             }
           });
@@ -85,17 +105,17 @@ const jsonFile = path.join(__dirname, `file-list/${cred.courseName}.json`);
         page.removeAllListeners('response');
       }
       await browser.close();
-      await writeJsonFile(jsonFile, lessonTable);  
-    })()
+      await writeJsonFile(jsonFile, lessonTable);
+    })();
   }
-  
+
   await (async () => {
     const lessonTable = await loadJsonFile(jsonFile);
 
     for (let lesson in lessonTable) {
       const types = ['audio', 'video'];
       const mergedFiles = [];
-      const lessonDirectory = path.join(baseDirectory, lesson)
+      const lessonDirectory = path.join(baseDirectory, lesson);
       for (let type of types) {
         let directory = path.join(lessonDirectory, type);
         await pmkdirp(directory);
@@ -116,9 +136,9 @@ const jsonFile = path.join(__dirname, `file-list/${cred.courseName}.json`);
 
       // combine video with audio
       const outputFile = esc(path.join(baseDirectory, `${lesson}.mp4`));
-      const cmd = `MP4Box -add ${mergedFiles[0]} -add ${mergedFiles[1]} -new ${outputFile}`
+      const cmd = `MP4Box -add ${mergedFiles[0]} -add ${mergedFiles[1]} -new ${outputFile}`;
       await pexec(cmd);
       await pexec(`rm ${mergedFiles.join(' ')}`);
-    }  
+    }
   })();
 })();
